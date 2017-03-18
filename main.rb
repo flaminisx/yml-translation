93066aacfccd5e44fca70fca0faeb38c53a6a78c
Igor Onischenko <optimum.flaminis@gmail.com>
2017-03-18 23:20:17 +0200

    yml file added

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require 'dotenv'
 require 'yandex-translator'
+require 'yaml'
 
 Dotenv.load
 
 translator = Yandex::Translator.new(ENV['YANDEX_API_KEY'])
+# puts translator.translate 'Car', from: 'en', to: 'ru'
 
-puts translator.translate 'Car', from: 'en', to: 'ru'
+file = YAML.load_file('tmp/input.yml')
