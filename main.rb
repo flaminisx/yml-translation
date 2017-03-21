8d6036fe3cc09cbbae51501e5693c1e10ea0e7c8
jenrenia <z1x212qw@mail.ru>
2017-03-21 04:57:46 +0200

    first working (not really) version of this tool

--- main.rb
+++ main.rb
@@ -4,7 +4,21 @@ require 'yaml'
 
 Dotenv.load
 
-translator = Yandex::Translator.new(ENV['YANDEX_API_KEY'])
+$translator = Yandex::Translator.new(ENV['YANDEX_API_KEY'])
 # puts translator.translate 'Car', from: 'en', to: 'ru'
 
 file = YAML.load_file('tmp/input.yml')
+
+def through_every_value(obj)
+  temp = {}
+  obj.each do |k, v|
+    if v.is_a?(Hash)
+      temp[k] = through_every_value(v)
+    else
+      temp[k] = $translator.translate v, from: 'en', to: 'ru'
+    end
+  end
+  temp
+end
+
+File.open('tmp/output.yml', 'w+') {|f| f.write through_every_value(file).to_yaml } 
